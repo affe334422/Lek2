2066f7997b0766b0fd79f81628219edbbd5ce921
affe334422 <alle07002@utb.vaxjo.se>
2026-01-13 14:08:47 +0100

    l

--- obj/Debug/net8.0-windows/Lek2.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Lek2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lek2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+badb45076c86c9a06a0f8ef18c5f0bea44322c40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2779bec6f68df6888d498d756c9c22e5c0ffb953")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lek2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lek2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
